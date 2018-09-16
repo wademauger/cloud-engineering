67452000a5e026110b5226ddea23bc51261eb33e
Wade Mauger <wadeanthony0100@gmail.com>
2018-09-16 10:18:07 -0400

    Fix express app

--- AWS-traffic-server/src/index.js
+++ AWS-traffic-server/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.json()); 
+
 app.get('/', (req, res) => {
   res.send('Sent POSTs pls.');
 });
 
 app.post('/rev', (req, res) => {
-  res.send(req.body.split('').reverse().join(''))
+  res.send(req.body.string.split('').reverse().join(''))
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
